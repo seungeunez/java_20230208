2a1c01a9d6ca3edf9f5d631e1077e93d56914d62
탁승은 <xkrtmddms@gmail.com>
2023-02-08 09:21:31 +0900

    DB

--- src/day8/BookInsertFrame.java
+++ src/day8/BookInsertFrame.java
@@ -1,40 +1,47 @@
 package day8;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton; 
+import javax.swing.JTextField; 
 
 public class BookInsertFrame extends JDialog {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField textField;	//제목
+	private JTextField textField_1;	//저자
+	private JTextField textField_2; //가격
+	private JTextField textField_3;	//분류
+
+	private JButton btnNewButton; //등록하기
+	
+	
 	public BookInsertFrame() {
 		getContentPane().setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(93, 32, 116, 21);
+		textField.setBounds(80, 65, 116, 21);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(93, 63, 116, 21);
+		textField_1.setBounds(80, 96, 116, 21);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(93, 94, 116, 21);
+		textField_2.setBounds(80, 127, 116, 21);
 		getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(93, 125, 116, 21);
+		textField_3.setBounds(80, 158, 116, 21);
 		getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(132, 202, 97, 23);
+		btnNewButton = new JButton("등록하기"); //밖으로 뺐음 안나와 있길래	
+		btnNewButton.setBounds(90, 204, 97, 23);
 		getContentPane().add(btnNewButton);
+	
+		this.setSize(300, 400);	//사이즈 설정
+		this.setVisible(true); 
+		
 	}
 }
--- src/day8/BookMain.java
+++ src/day8/BookMain.java
@@ -5,6 +5,9 @@ package day8;
 public class BookMain {
 
 	public static void main(String[] args) {
+		new BookInsertFrame();	//사용할게 아니라서 생성자명 선언하는건 안했음  BookInsertFrame bif = 이런거 말이야
+		
+		
 		
 
 	}
